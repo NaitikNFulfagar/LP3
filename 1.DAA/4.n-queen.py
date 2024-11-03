6a9242bf17067c2b011a55d939c8fa99fe26553b
Naitik Fulfagar <naitiknfulfagar@gmail.com>
2024-11-03 08:51:17 +0530

    newwwwwww

--- 1.DAA/4.n-queen.py
+++ 1.DAA/4.n-queen.py
@@ -1,50 +1,59 @@
-def print_solution(board):
+def print_board(board):
     for row in board:
-        print(" ".join("Q" if col else "." for col in row))
+        print(" ".join(str(x) for x in row))
     print()
 
-
-def is_safe(board, row, col):
-    # Check this column on upper side
+def is_safe(board, row, col, n):
     for i in range(row):
-        if board[i][col]:
+        if board[i][col] == 1:
             return False
 
-    # Check upper diagonal on left side
-    for i, j in zip(range(row, -1, -1), range(col, -1, -1)):
-        if j < 0:  # Check column bounds
-            break
-        if board[i][j]:
+    i, j = row, col
+    while i >= 0 and j >= 0:
+        if board[i][j] == 1:
             return False
+        i -= 1
+        j -= 1
 
-    # Check upper diagonal on right side
-    for i, j in zip(range(row, -1, -1), range(col, len(board))):
-        if j >= len(board):  # Check column bounds
-            break
-        if board[i][j]:
+    i, j = row, col
+    while i >= 0 and j < n:
+        if board[i][j] == 1:
             return False
+        i -= 1
+        j += 1
 
     return True
 
+def solve_nqueens(board, row, n):
+    if row >= n:
+        return True
+    
+    for col in range(n):
+        if is_safe(board, row, col, n):
+            board[row][col] = 1
 
-def solve_n_queens_util(board, row):
-    if row >= len(board):
-        print_solution(board)
-        return
+            if solve_nqueens(board, row + 1, n):
+                return True
+            board[row][col] = 0
+            
+    return False
 
-    for col in range(len(board)):
-        if is_safe(board, row, col):
-            board[row][col] = True  # Place queen
-            solve_n_queens_util(board, row + 1)  # Recur to place rest of the queens
-            board[row][col] = False  # Backtrack
+def n_queens_with_first_queen(n, first_row, first_col):
+    board = [[0] * n for _ in range(n)]
+    board[first_row][first_col] = 1
+
+    if solve_nqueens(board, first_row + 1, n):
+        print("Solution found:")
+        print_board(board)
+    else:
+        print("No solution exists for this configuration.")
 
 
-def solve_n_queens(n):
-    board = [[False] * n for _ in range(n)]  # Create an n x n board
-    solve_n_queens_util(board, 0)
+n         = int(input("Enter the size of the chessboard (n): "))
+first_row = int(input("Enter the row index of the first queen (0 to n-1): "))
+first_col = int(input("Enter the column index of the first queen (0 to n-1): "))
 
-
-# Main program
-if __name__ == "__main__":
-    n = int(input("Enter the number of queens: "))
-    solve_n_queens(n)
+if 0 <= first_row < n and 0 <= first_col < n:
+    n_queens_with_first_queen(n, first_row, first_col)
+else:
+    print("Invalid position for the first queen.")
